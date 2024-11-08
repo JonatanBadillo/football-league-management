fff018ebf26a1004c483f40c20ef111cee18a234
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-11-08 10:36:51 -0600

    handlerbars

--- webapp/src/server.ts
+++ webapp/src/server.ts
@@ -10,7 +10,8 @@ const PORT = 3000;
 // Configuración de Handlebars
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../src/views'));
+
 
 // Middleware para archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
