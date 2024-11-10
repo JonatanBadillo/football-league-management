940c138d33cd23f971272369185bfa9a20b59266
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-11-09 22:11:00 -0600

    Actualizar tamaño de imágenes de logos en las vistas home y next_matches

--- webapp/src/server.ts
+++ webapp/src/server.ts
@@ -1,48 +1,52 @@
-
-import moment from 'moment';
-import express from 'express';
-import fs from 'fs';
-import https from 'https';
-import path from 'path';
-import { engine } from 'express-handlebars';
-import adminRoutes from './routes/adminRoutes';
-import multer from 'multer';
-import sequelize from './config/database';
-import League from './model/League';
-import Team from './model/Team';
-import Match from './model/Match';
+import moment from "moment";
+import express from "express";
+import fs from "fs";
+import https from "https";
+import path from "path";
+import { engine } from "express-handlebars";
+import adminRoutes from "./routes/adminRoutes";
+import multer from "multer";
+import sequelize from "./config/database";
+import League from "./model/League";
+import Team from "./model/Team";
+import Match from "./model/Match";
+import Player from "./model/Player";
 
 const app = express();
 const PORT = 3000;
 
 // Configuración de Handlebars con opción para acceder a propiedades
-app.engine('handlebars', engine({
-  defaultLayout: 'main',
-  runtimeOptions: {
-    allowProtoPropertiesByDefault: true,
-    allowProtoMethodsByDefault: true
-  },
-  helpers: {
-    subtract: (a: number, b: number) => a - b,
-    increment: (index: number) => index + 1,  
-    formatDate: (date: moment.MomentInput) => moment(date).format('MMM DD, h:mm A'), // Helper para formatear fechas
-  },
-}));
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+    },
+    helpers: {
+      subtract: (a: number, b: number) => a - b,
+      increment: (index: number) => index + 1,
+      formatDate: (date: moment.MomentInput) =>
+        moment(date).format("MMM DD, h:mm A"), // Helper para formatear fechas
+    },
+  })
+);
 
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, '../src/views'));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../src/views"));
 
 // Middleware para manejar JSON y archivos estáticos
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../src/public')));
+app.use(express.static(path.join(__dirname, "../src/public")));
 
 // Cargar las rutas de administración sin autorización
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 
 // Configuración de Multer para la carga de archivos
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
@@ -51,34 +55,48 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Ruta de carga de imágenes
-app.post('/upload-player-image', upload.single('image'), (req, res) => {
+app.post("/upload-player-image", upload.single("image"), (req, res) => {
   if (req.file) {
     res.json({ imageUrl: `/uploads/${req.file.filename}` });
   } else {
-    res.status(400).json({ error: 'No se pudo subir la imagen' });
+    res.status(400).json({ error: "No se pudo subir la imagen" });
   }
 });
 
 // Ruta de la página principal para cargar ligas, equipos y próximos partidos
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const leagues = await League.findAll();
     const teams = await Team.findAll({
-      order: [['points', 'DESC']],
+      order: [["points", "DESC"]],
     });
-    const matches = await Match.findAll({
-      order: [['date', 'ASC']],
+
+    const topScorers = await Player.findAll({
+      order: [["goals", "DESC"]],
+      limit: 5, // Limitar a los 5 primeros jugadores
       include: [
-        { model: Team, as: 'homeTeam' },
-        { model: Team, as: 'awayTeam' },
+        {
+          model: Team,
+          as: "team",
+          required: true,
+        },
       ],
     });
 
-    res.render('home', {
-      title: 'Football League Management',
+    const matches = await Match.findAll({
+      order: [["date", "ASC"]],
+      include: [
+        { model: Team, as: "homeTeam" },
+        { model: Team, as: "awayTeam" },
+      ],
+    });
+
+    res.render("home", {
+      title: "Football League Management",
       leagues,
       teams,
       matches,
+      topScorers,
     });
   } catch (error) {
     console.error("Error al obtener datos para la página principal:", error);
@@ -86,21 +104,21 @@ app.get('/', async (req, res) => {
   }
 });
 
-
 // Opciones SSL para HTTPS
 const sslOptions = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
 };
 
 // Sincronizar la base de datos y luego iniciar el servidor HTTPS
-sequelize.sync()
+sequelize
+  .sync()
   .then(() => {
-    console.log('Base de datos sincronizada');
+    console.log("Base de datos sincronizada");
     https.createServer(sslOptions, app).listen(PORT, () => {
       console.log(`Server is running on https://localhost:${PORT}`);
     });
   })
   .catch((error: any) => {
-    console.error('Error al conectar a la base de datos:', error);
+    console.error("Error al conectar a la base de datos:", error);
   });
